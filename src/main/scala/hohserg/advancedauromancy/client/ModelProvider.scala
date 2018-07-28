267f9c9b0c3a8d82eb7db56589b32087fd6e7bd6
hohserg <hohserg1@yandex.ru>
2018-07-28 13:06:47 +0300

    Trait for share model

---
+++ src/main/scala/hohserg/advancedauromancy/client/ModelProvider.scala
@@ -0,0 +1,8 @@
+package hohserg.advancedauromancy.client
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation
+
+trait ModelProvider {
+  def location:ModelResourceLocation
+
+}
