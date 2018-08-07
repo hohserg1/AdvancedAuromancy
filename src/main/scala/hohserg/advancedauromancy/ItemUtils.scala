a361a37dcf1cadf4a98a1daefcd995b9d57ba600
hohserg <hohserg1@yandex.ru>
2018-08-08 00:27:20 +0300

    Update item utils

---
+++ src/main/scala/hohserg/advancedauromancy/ItemUtils.scala
@@ -0,0 +1,13 @@
+package hohserg.advancedauromancy
+
+import net.minecraft.item.{Item, ItemStack}
+
+object ItemUtils {
+  def getItemKey(item: Item): String = item.delegate.name().toString
+
+  def equalsStacks(_1: ItemStack, _2: ItemStack): Boolean = Option(_1).flatMap(i=>Option(_2).map(j=>i.getItem==j.getItem && i.getItemDamage==j.getItemDamage)).getOrElse(false)
+
+  def getItemStackKey(itemStack: ItemStack): String = itemStack.getItem.delegate.name().toString
+
+
+}
