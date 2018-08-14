898bf5c3cf0afaf3735c288529f93e8dc2844225
hohserg <hohserg1@yandex.ru>
2018-08-14 22:03:50 +0300

    Added new wand rods

--- src/main/scala/hohserg/advancedauromancy/wands/RodsAndCaps.scala
+++ src/main/scala/hohserg/advancedauromancy/wands/RodsAndCaps.scala
@@ -11,32 +11,39 @@ import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.{Item, ItemStack}
 import net.minecraft.util.{EnumFacing, ResourceLocation}
+import net.minecraft.world.storage.loot.LootTable
 import net.minecraftforge.fml.common.registry.ForgeRegistries
 
 import scala.collection.mutable
 
 object RodsAndCaps{
   val identityOnUpdate: (ItemStack, EntityPlayer) => Unit = WandRod.identityOnUpdate
+  LootTable
   lazy val testUpgrade=WandUpgrade.register(0,1,"TestUpgrade",100,identityOnUpdate,new IBakedSource {
     override def getQuads(state: IBlockState, side: EnumFacing, rand: Long): util.List[BakedQuad] = util.Collections.emptyList()
   })
   def initRodAndCaps() = {
-    WandCap.register(0.7f,"GoldCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
-    WandCap.register(0.7f,"ThaumiumCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
-    WandCap.register(0.7f,"VoidCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
-    WandCap.register(0.7f,"AurumCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
+    WandCap.register(0.7f,"GoldCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_gold"))
+    WandCap.register(0.7f,"ThaumiumCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_thaumium"))
+    WandCap.register(0.7f,"VoidCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_void"))
+    WandCap.register(0.7f,"AuramCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_aurum"))
     WandCap.register(0.7f,"EnderCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
-    WandCap.register(0.7f,"CrystalCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
-    WandCap.register(0.7f,"FairyCap",100,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_cap_ender"))
 
     testUpgrade
 
     WandRod.register(100, "GreatwoodRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_greatwood"))
-
     WandRod.register(100, "SilverwoodRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_silverwood"))
+    WandRod.register(100, "TaintwoodRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_taintrwood"))
+
+    WandRod.register(100, "BirchRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_birchwood"))
+    WandRod.register(100, "OakRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_oakwood"))
+    WandRod.register(100, "SpruceRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_sprucewood"))
+    WandRod.register(100, "JungleRod",0,identityOnUpdate,new ResourceLocation(advancedAuromancyModId+":rods_and_caps/wand_rod_junglewood"))
 
     DefaultCap
     DefaultRod
+
+    WandUpgrade.register(0,1,"ChargeIndicator",10,identityOnUpdate,IBakedSource.empty)
 
   }
 
