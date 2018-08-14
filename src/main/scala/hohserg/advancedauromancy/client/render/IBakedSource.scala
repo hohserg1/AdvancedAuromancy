3967c2496663bc17a2657bb1933575f82fc6ce0a
hohserg <hohserg1@yandex.ru>
2018-08-14 22:02:44 +0300

    Added empty IBakedSource

--- src/main/scala/hohserg/advancedauromancy/client/render/IBakedSource.scala
+++ src/main/scala/hohserg/advancedauromancy/client/render/IBakedSource.scala
@@ -1,9 +1,13 @@
 package hohserg.advancedauromancy.client.render
 
+import java.util
+
 import javax.vecmath.Matrix4f
+import net.minecraft.block.state.IBlockState
 import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.block.model.{IBakedModel, ItemCameraTransforms, ItemOverrideList}
+import net.minecraft.client.renderer.block.model.{BakedQuad, IBakedModel, ItemCameraTransforms, ItemOverrideList}
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
+import net.minecraft.util.EnumFacing
 import org.apache.commons.lang3.tuple.Pair
 
 trait IBakedSource extends IBakedModel{
@@ -22,5 +26,12 @@ trait IBakedSource extends IBakedModel{
   override def handlePerspective(cameraTransformType: ItemCameraTransforms.TransformType): Pair[_ <: IBakedModel, Matrix4f] =
     Pair.of(this, new Matrix4f())
 
+
+}
+
+object IBakedSource {
+  val empty: IBakedSource =new IBakedSource {
+    override def getQuads(state: IBlockState, side: EnumFacing, rand: Long): util.List[BakedQuad] = util.Collections.emptyList()
+  }
 
 }
