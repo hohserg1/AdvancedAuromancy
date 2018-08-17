7d7392e3c5585520b41d925b626a54df968dc2df
hohserg <hohserg1@yandex.ru>
2018-08-17 11:59:14 +0300

    Refactoring

--- src/main/scala/hohserg/advancedauromancy/items/ItemSenseGoggles.scala
+++ src/main/scala/hohserg/advancedauromancy/items/ItemSenseGoggles.scala
@@ -5,8 +5,8 @@ import net.minecraft.util.ResourceLocation
 import thaumcraft.common.items.armor.ItemGoggles
 
 object ItemSenseGoggles extends ItemGoggles{
-  private val tex=classOf[ItemGoggles].getDeclaredField("tex")
-  tex.setAccessible(true)
-  tex.set(this,new ResourceLocation(Main.advancedAuromancyModId, "textures/items/goggles_bauble.png"))
+  private val texField=classOf[ItemGoggles].getDeclaredField("tex")
+  texField.setAccessible(true)
+  texField.set(this,new ResourceLocation(Main.advancedAuromancyModId, "textures/items/goggles_bauble.png"))
 
 }
