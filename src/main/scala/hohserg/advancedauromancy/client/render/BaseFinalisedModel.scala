230546663f50a03f5a337d5b2a36e0b706efcd85
hohserg <hohserg1@yandex.ru>
2018-08-22 10:02:41 +0300

    Extract duplicates

---
+++ src/main/scala/hohserg/advancedauromancy/client/render/BaseFinalisedModel.scala
@@ -0,0 +1,79 @@
+package hohserg.advancedauromancy.client.render
+
+import javax.vecmath.Matrix4f
+import net.minecraft.client.renderer.block.model.{IBakedModel, ItemCameraTransforms}
+import net.minecraft.client.renderer.texture.TextureAtlasSprite
+import org.apache.commons.lang3.tuple.Pair
+
+import scala.math.{cos, sin, toRadians}
+
+trait BaseFinalisedModel extends IBakedModel {
+  def parentModel: IBakedModel
+  override def isAmbientOcclusion: Boolean = parentModel.isAmbientOcclusion
+
+  override def isGui3d: Boolean = parentModel.isGui3d
+
+  override def isBuiltInRenderer: Boolean = parentModel.isBuiltInRenderer
+
+  override def getParticleTexture: TextureAtlasSprite = parentModel.getParticleTexture
+
+  override def getItemCameraTransforms: ItemCameraTransforms = parentModel.getItemCameraTransforms
+
+  def identity: Matrix4f = {
+    val m = new Matrix4f()
+    m.setIdentity()
+    m
+  }
+
+  def rotateY(i: Float): Matrix4f = {
+    val m = identity
+    m.m00 = cos(toRadians(i)).toFloat
+    m.m02 = sin(toRadians(i)).toFloat
+    m.m20 = -sin(toRadians(i)).toFloat
+    m.m22 = cos(toRadians(i)).toFloat
+    m
+  }
+
+  def rotateZ(i: Double): Matrix4f = {
+    val m = identity
+    m.m00 = cos(toRadians(i)).toFloat
+    m.m01 = -sin(toRadians(i)).toFloat
+    m.m10 = sin(toRadians(i)).toFloat
+    m.m11 = cos(toRadians(i)).toFloat
+    m
+  }
+
+  def scale(x: Double, y: Double, z: Double): Matrix4f = {
+    val m = identity
+    m.m00 = x.toFloat
+    m.m11 = y.toFloat
+    m.m22 = z.toFloat
+    m
+  }
+
+  def scale(d: Double): Matrix4f = {
+    val m = identity
+    m.m00 = d.toFloat
+    m.m11 = d.toFloat
+    m.m22 = d.toFloat
+    m
+  }
+
+  def move(x: Double, y: Double, z: Double): Matrix4f = {
+    val m = identity
+    m.m03 = x.toFloat
+    m.m13 = y.toFloat
+    m.m23 = z.toFloat
+    m
+  }
+
+  override def handlePerspective(cameraTransformType: ItemCameraTransforms.TransformType): Pair[_ <: IBakedModel, Matrix4f] = {
+    def matrixOrIdentity(o: Option[Matrix4f]) = o.getOrElse(identity)
+
+    val mat = matrixOrIdentity(Option(parentModel.handlePerspective(cameraTransformType).getRight))
+    Pair.of(this, mat)
+  }
+
+  override def getOverrides = throw new UnsupportedOperationException("The finalised model does not have an override list.")
+
+}
--- src/main/scala/hohserg/advancedauromancy/client/render/wand/WandFinalisedModel.scala
+++ src/main/scala/hohserg/advancedauromancy/client/render/wand/WandFinalisedModel.scala
@@ -2,21 +2,18 @@ package hohserg.advancedauromancy.client.render.wand
 
 import java.util
 
-import hohserg.advancedauromancy.client.render.Cube
+import hohserg.advancedauromancy.client.render.{BaseFinalisedModel, Cube}
 import hohserg.advancedauromancy.wands._
-import javax.vecmath.Matrix4f
 import net.minecraft.block.state.IBlockState
 import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.block.model.{BakedQuad, IBakedModel, ItemCameraTransforms}
+import net.minecraft.client.renderer.block.model.{BakedQuad, IBakedModel}
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.util.EnumFacing
 import net.minecraftforge.client.model.pipeline.UnpackedBakedQuad
-import org.apache.commons.lang3.tuple.Pair
 
 import scala.collection.mutable
-import scala.math.{cos, sin, toRadians}
 
-class WandFinalisedModel(parentModel: IBakedModel, key: (WandRod, WandCap, Option[Int], List[WandUpgrade])) extends IBakedModel {
+class WandFinalisedModel(val parentModel: IBakedModel, key: (WandRod, WandCap, Option[Int], List[WandUpgrade])) extends BaseFinalisedModel {
 
   override def getQuads(state: IBlockState, side: EnumFacing, rand: Long): util.List[BakedQuad] = {
     if (side != null) parentModel.getQuads(state, side, rand)
@@ -33,124 +30,6 @@ class WandFinalisedModel(parentModel: IBakedModel, key: (WandRod, WandCap, Optio
       r
     }
   }
-
-  override def isAmbientOcclusion: Boolean = parentModel.isAmbientOcclusion
-
-  override def isGui3d: Boolean = parentModel.isGui3d
-
-  override def isBuiltInRenderer: Boolean = parentModel.isBuiltInRenderer
-
-  override def getParticleTexture: TextureAtlasSprite = parentModel.getParticleTexture
-
-  override def getItemCameraTransforms: ItemCameraTransforms = parentModel.getItemCameraTransforms
-
-  def identity: Matrix4f = {
-    val m = new Matrix4f()
-    m.setIdentity()
-    m
-  }
-
-  def rotateY(i: Float): Matrix4f = {
-    val m = identity
-    m.m00 = cos(toRadians(i)).toFloat
-    m.m02 = sin(toRadians(i)).toFloat
-    m.m20 = -sin(toRadians(i)).toFloat
-    m.m22 = cos(toRadians(i)).toFloat
-    m
-  }
-
-  def rotateZ(i: Double): Matrix4f = {
-    val m = identity
-    m.m00 = cos(toRadians(i)).toFloat
-    m.m01 = -sin(toRadians(i)).toFloat
-    m.m10 = sin(toRadians(i)).toFloat
-    m.m11 = cos(toRadians(i)).toFloat
-    m
-  }
-
-  def scale(x: Double, y: Double, z: Double): Matrix4f = {
-    val m = identity
-    m.m00 = x.toFloat
-    m.m11 = y.toFloat
-    m.m22 = z.toFloat
-    m
-  }
-
-  def scale(d: Double): Matrix4f = {
-    val m = identity
-    m.m00 = d.toFloat
-    m.m11 = d.toFloat
-    m.m22 = d.toFloat
-    m
-  }
-
-  def move(x: Double, y: Double, z: Double): Matrix4f = {
-    val m = identity
-    m.m03 = x.toFloat
-    m.m13 = y.toFloat
-    m.m23 = z.toFloat
-    m
-  }
-
-  override def handlePerspective(cameraTransformType: ItemCameraTransforms.TransformType): Pair[_ <: IBakedModel, Matrix4f] = {
-    def matrixOrIdentity(o: Option[Matrix4f]) = o.getOrElse(identity)
-
-    val mat = matrixOrIdentity(Option(parentModel.handlePerspective(cameraTransformType).getRight))
-
-    //Moved to json
-
-    //val a = -4 * -0.5*(1-854/Minecraft.getMinecraft.displayWidth)
-    /*
-    cameraTransformType match {
-      case TransformType.THIRD_PERSON_LEFT_HAND =>
-        //mat mul scale(0.2)
-        //mat mul move(-0.7,-1,0)
-        //mat mul rotateY(45)
-        //mat mul move(0.5,0,0.5)
-
-      case TransformType.THIRD_PERSON_RIGHT_HAND =>
-        //mat mul scale(0.2)
-        //mat mul move(0,-1,-0.7)
-        //mat mul rotateY(45)
-        //mat mul move(0.5,0,0.5)
-
-      case TransformType.FIRST_PERSON_RIGHT_HAND =>
-
-        //mat mul move(-0.5,0,-0.5)
-        //mat mul rotateY(-30)
-        //mat mul move(0.5,0,0.5)
-
-        //mat mul scale(0.15)
-        //mat mul move(1,-0.5,-a)
-        //mat mul rotateY(-30)
-        //mat mul rotateZ(25)
-
-      case TransformType.FIRST_PERSON_LEFT_HAND =>
-        //mat mul rotateZ(-25)
-
-        //mat mul move(0.5,0,0.5)
-        //mat mul rotateY(-30)
-        //mat mul move(-0.5,0,-0.5)
-
-        //mat mul scale(0.15)
-        //mat mul move(-1,-0.5,a+1)
-        //mat mul move(-0.7f,-0.5,2f)
-
-      case TransformType.GROUND =>
-        //mat mul scale(0.3)
-        //mat mul move(0.5,-1f,0.5)
-
-      case TransformType.GUI =>
-        //mat mul rotateZ(-135)
-        //mat mul rotateY(45)
-        //mat mul scale(0.2f)
-        //mat mul move(0.5,-1.5f,0.5)
-      case _=>
-    }*/
-    Pair.of(this, mat)
-  }
-
-  override def getOverrides = throw new UnsupportedOperationException("The finalised model does not have an override list.")
 }
 
 object WandFinalisedModel {
