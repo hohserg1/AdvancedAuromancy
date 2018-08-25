341942028822012239c265455bd4909666eb2cbb
hohserg <hohserg1@yandex.ru>
2018-08-25 13:52:32 +0300

    Refactoring and added self register item client registry

--- src/main/scala/hohserg/advancedauromancy/items/base/ItemSelfRegister.scala
+++ src/main/scala/hohserg/advancedauromancy/items/base/ItemSelfRegister.scala
@@ -1,29 +1,20 @@
 package hohserg.advancedauromancy.items.base
 
 import hohserg.advancedauromancy.Main
-import hohserg.advancedauromancy.client.ModelProvider
-import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.block.model.ModelResourceLocation
+import hohserg.advancedauromancy.client.ClientEventHandler
+import hohserg.advancedauromancy.client.render.simpleItem.SimpleTexturedModelProvider
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
-import net.minecraftforge.fml.common.FMLCommonHandler
-import net.minecraftforge.fml.relauncher.Side
+import net.minecraft.util.ResourceLocation
 
-abstract class ItemSelfRegister extends Item{
+abstract class ItemSelfRegister extends Item with SimpleTexturedModelProvider{
   def name:String
+
   setRegistryName(name)
   setUnlocalizedName(name)
   CreativeTabs.CREATIVE_TAB_ARRAY.find(ct => ct.getTabLabel == Main.advancedAuromancyModId).foreach(setCreativeTab)
-  if(FMLCommonHandler.instance().getEffectiveSide==Side.CLIENT) {
-    this match{
-      case item: ModelProvider =>
-        val model = item.location
-        Minecraft.getMinecraft.getRenderItem.getItemModelMesher.register(this, 0, model)
-      case item =>
-        val model = new ModelResourceLocation(name, "inventory")
-        Minecraft.getMinecraft.getRenderItem.getItemModelMesher.register(this, 0, model)
+  Main.proxy.markForRegisterClient(this)
 
-    }
-  }
-
+  override val textureName: String = Main.advancedAuromancyModId+":items/"+name
+  ClientEventHandler.registerTexture(new ResourceLocation(textureName))
 }
