4c4d1057721afe9d25d465ab49f83ca6e7c19f1c
hohserg <hohserg1@yandex.ru>
2018-08-25 13:51:39 +0300

    Added wand name

--- src/main/scala/hohserg/advancedauromancy/client/TooltipHandler.scala
+++ src/main/scala/hohserg/advancedauromancy/client/TooltipHandler.scala
@@ -1,6 +1,7 @@
 package hohserg.advancedauromancy.client
 
 import hohserg.advancedauromancy.items.base.Wand
+import net.minecraft.util.text.translation.I18n
 import net.minecraftforge.event.entity.player.ItemTooltipEvent
 import net.minecraftforge.fml.common.eventhandler.{EventPriority, SubscribeEvent}
 
@@ -13,6 +14,7 @@ class TooltipHandler {
       case wand: Wand =>
         //if(stack.getItem==ItemEnderWandCasting)
        //   e.getToolTip add "§bEnder vis net owner is "+EnderVisNetServer.getName(stack).getOrElse("")
+        e.getToolTip.set(0, I18n.translateToLocal(wand.getCap(stack).name)+" "+I18n.translateToLocal(wand.getRod(stack).name))
         e.getToolTip.set(e.getToolTip.asScala.indexWhere((param: String) => param.contains("Vis charge")),"Vis charge: "+wand.getVis(stack))
       case _ =>
     }
