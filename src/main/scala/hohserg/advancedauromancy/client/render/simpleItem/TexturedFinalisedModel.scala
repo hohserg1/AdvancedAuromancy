c5d2fbecf650cdba5572cc893502ee6084a02ef3
hohserg <hohserg1@yandex.ru>
2018-08-25 13:54:28 +0300

    Resealed TexturedFinalisedModel

--- src/main/scala/hohserg/advancedauromancy/client/render/simpleItem/TexturedFinalisedModel.scala
+++ src/main/scala/hohserg/advancedauromancy/client/render/simpleItem/TexturedFinalisedModel.scala
@@ -1,19 +1,32 @@
 package hohserg.advancedauromancy.client.render.simpleItem
 
 import java.util
+import java.util.Collections
 
 import hohserg.advancedauromancy.client.render.BaseFinalisedModel
 import net.minecraft.block.state.IBlockState
-import net.minecraft.client.renderer.block.model.{BakedQuad, IBakedModel}
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.block.model._
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.util.EnumFacing
 
 class TexturedFinalisedModel(val parentModel: IBakedModel, key:String)  extends BaseFinalisedModel {
 
-  override def getQuads(state: IBlockState, side: EnumFacing, rand: Long): util.List[BakedQuad] = {
-    if (side != null) parentModel.getQuads(state, side, rand)
-    else {
-      parentModel.getQuads(state, side, rand)
+  import java.util.Optional
 
-    }
+  import com.google.common.collect.ImmutableList
+  import net.minecraft.client.renderer.block.model.BakedQuad
+  import net.minecraft.client.renderer.texture.TextureAtlasSprite
+  import net.minecraftforge.client.model.ItemLayerModel
+
+  val textureAtlasSprite: TextureAtlasSprite = Minecraft.getMinecraft.getTextureMapBlocks.getAtlasSprite(key)
+  val quads: ImmutableList[BakedQuad] = ItemLayerModel.getQuadsForSprite(0, textureAtlasSprite,DefaultVertexFormats.ITEM,Optional.empty())
+  println(key,textureAtlasSprite)
+
+  override def getQuads(state: IBlockState, side: EnumFacing, rand: Long): util.List[BakedQuad] = {
+    if (side != null)
+      Collections.emptyList()
+    else
+      quads
   }
 }
