c6a471fc791facbeb3ce14ecc146332e2fbf2a82
hohserg <hohserg1@yandex.ru>
2018-08-31 15:46:25 +0300

    Refactoring

--- src/main/scala/hohserg/advancedauromancy/client/TooltipHandler.scala
+++ src/main/scala/hohserg/advancedauromancy/client/TooltipHandler.scala
@@ -17,7 +17,7 @@ class TooltipHandler {
     stack.getItem match {
       case wand: Wand =>
         if(stack.getItem==ItemEnderWandCasting)
-          e.getToolTip add "§bEnder vis net owner is "+Main.proxy.enderVisNet.getName(stack).getOrElse("")
+          e.getToolTip add TextFormatting.AQUA+"Ender vis net owner is "+Main.proxy.enderVisNet.getName(stack).getOrElse("")
         e.getToolTip.set(0, I18n.translateToLocal(wand.getCap(stack).name)+" "+I18n.translateToLocal(wand.getRod(stack).name))
         e.getToolTip.set(e.getToolTip.asScala.indexWhere((param: String) => param.contains(visChargeLabel)),TextFormatting.YELLOW+visChargeLabel + " "+wand.getVis(stack))
       case _ =>
