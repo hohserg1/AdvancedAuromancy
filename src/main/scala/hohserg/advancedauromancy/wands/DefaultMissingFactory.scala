e6d31a15930715c5bf96d3b0d353f7baf201b702
hohserg <hohserg1@yandex.ru>
2019-07-14 17:23:22 +0300

    Remove unused

--- src/main/scala/hohserg/advancedauromancy/wands/DefaultMissingFactory.scala
+++
@@ -1,8 +0,0 @@
-package hohserg.advancedauromancy.wands
-
-import net.minecraft.util.ResourceLocation
-import net.minecraftforge.registries.IForgeRegistry.MissingFactory
-
-class DefaultMissingFactory[A <: WandComponentRegistryEntry[A]](value: A) extends MissingFactory[A] {
-  override def createMissing(key: ResourceLocation, isNetwork: Boolean): A = value
-}
