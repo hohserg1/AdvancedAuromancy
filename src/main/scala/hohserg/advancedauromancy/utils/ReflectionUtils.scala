30d6a98681899ac6f12390304d975beb1cbc679c
hohserg <hohserg1@yandex.ru>
2021-03-15 03:50:58 +0300

    Fix ReflectionUtils

--- src/main/scala/hohserg/advancedauromancy/utils/ReflectionUtils.scala
+++ src/main/scala/hohserg/advancedauromancy/utils/ReflectionUtils.scala
@@ -1,14 +1,12 @@
 package hohserg.advancedauromancy.utils
 
-import thaumcraft.common.lib.network.PacketHandler
-
 import scala.reflect.ClassTag
 
 object ReflectionUtils {
   def getPrivateField[CL: ClassTag, A](value: CL, field: String): A = {
     val f = implicitly[ClassTag[CL]].runtimeClass.getDeclaredField(field)
     f.setAccessible(true)
-    f.get(PacketHandler.INSTANCE).asInstanceOf[A]
+    f.get(value).asInstanceOf[A]
   }
 
 }
